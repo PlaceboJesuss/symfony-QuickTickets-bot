0d8c01e3eb4359f834d776ee332c889faeb1015c
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-24 23:19:44 +0300

    fix nginx config

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,13 @@
 set -e
 
 echo "🔧 Generating Nginx config"
-envsubst '${APP_DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+if [ "$APP_ENV" = "prod" ]; then
+  TEMPLATE=/etc/nginx/conf.d/prod.template
+else
+  TEMPLATE=/etc/nginx/conf.d/dev.template
+fi
+
+envsubst '${APP_DOMAIN}' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
 
 echo "🚀 Starting nginx"
 exec nginx -g 'daemon off;'
