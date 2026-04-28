f61c25b4e4afc7685f19f9edc2760192310540a9
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-28 21:09:08 +0300

    test

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,51 @@
 #!/bin/sh
 set -e
 
-echo "🔧 Generating Nginx config"
-envsubst '${APP_DOMAIN}' < /etc/nginx/conf.d/tmp.conf > /etc/nginx/conf.d/default.conf
+#!/bin/sh
+set -e
 
-echo "🚀 Starting nginx"
-exec nginx -g 'daemon off;'
+CERT_PATH="/etc/letsencrypt/live/${APP_DOMAIN}/fullchain.pem"
+
+echo "Checking SSL certificate..."
+
+if [ ! -f "$CERT_PATH" ]; then
+    echo "❌ Certificate not found → starting HTTP mode"
+
+    envsubst '${APP_DOMAIN}' \
+        < /etc/nginx/http.conf.template \
+        > /etc/nginx/conf.d/default.conf
+
+    nginx
+
+    echo "⏳ Waiting nginx to be ready..."
+    sleep 3
+
+    echo "🚀 Requesting certificate..."
+
+    certbot certonly \
+        --webroot \
+        --webroot-path=/var/www/certbot \
+        -d ${APP_DOMAIN} \
+        -d www.${APP_DOMAIN} \
+        --email ${LETSENCRYPT_EMAIL} \
+        --agree-tos \
+        --no-eff-email \
+        --non-interactive
+
+    echo "♻️ Reloading nginx with SSL..."
+
+    envsubst '${APP_DOMAIN}' \
+        < /etc/nginx/https.conf.template \
+        > /etc/nginx/conf.d/default.conf
+
+    nginx -s reload
+
+else
+    echo "✅ Certificate exists → starting HTTPS"
+
+    envsubst '${APP_DOMAIN}' \
+        < /etc/nginx/https.conf.template \
+        > /etc/nginx/conf.d/default.conf
+
+    exec nginx -g "daemon off;"
+fi
