fa00320d29b6ac1e946de2eab1b9c4cd77e81f1e
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-28 20:42:56 +0300

    test

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,13 +2,6 @@
 set -e
 
 echo "🔧 Generating Nginx config"
-if [ "$APP_ENV" = "prod" ]; then
-  TEMPLATE=/etc/nginx/conf.d/prod.template
-else
-  TEMPLATE=/etc/nginx/conf.d/dev.template
-fi
-
-envsubst '${APP_DOMAIN}' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
 
 echo "🚀 Starting nginx"
 exec nginx -g 'daemon off;'
