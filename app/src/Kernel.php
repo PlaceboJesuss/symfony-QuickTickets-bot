4954912724d5a5c6bea07e070da8fb72aa82639f
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-29 18:19:22 +0300

    test

--- app/src/Kernel.php
+++ app/src/Kernel.php
@@ -2,9 +2,6 @@
 
 namespace App;
 
-use App\Attributes\AsMessengerService;
-use App\CompilerPass\MessengerServicePass;
-use App\Telegram\CompilerPass\CallbackPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
