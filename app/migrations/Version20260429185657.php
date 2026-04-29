098c0ef34d88618d981f1507362fe2dad2227c24
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-29 22:18:31 +0300

    plz

--- app/migrations/Version20260429185657.php
+++ app/migrations/Version20260429185657.php
@@ -21,13 +21,36 @@ final class Version20260429185657 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
 
-        $this->addSql('ALTER TABLE `messenger_user` CHANGE `chat_id` `chat_id` BIGINT NOT NULL');
-        $this->addSql('ALTER TABLE `messenger_user` CHANGE `username` `username` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE place_messenger_user DROP FOREIGN KEY FK_16E31C451A9A7125676C7AF5');
+
+        // 2. меняем chat_id на BIGINT
+        $this->addSql('ALTER TABLE messenger_user MODIFY chat_id BIGINT NOT NULL');
+        $this->addSql('ALTER TABLE place_messenger_user MODIFY chat_id BIGINT NOT NULL');
+
+        // 3. делаем username nullable
+        $this->addSql('ALTER TABLE messenger_user MODIFY username VARCHAR(255) DEFAULT NULL');
+
+        // 4. возвращаем FK обратно
+        $this->addSql(
+            'ALTER TABLE place_messenger_user ADD CONSTRAINT FK_16E31C451A9A7125676C7AF5 FOREIGN KEY (chat_id, messenger_id) REFERENCES messenger_user (chat_id, messenger_id)'
+        );
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE `messenger_user` CHANGE `chat_id` `chat_id` INT(11) NOT NULL');
-        $this->addSql('ALTER TABLE `messenger_user` CHANGE `username` `username` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL');
+        // 1. убираем FK (как в up)
+        $this->addSql('ALTER TABLE place_messenger_user DROP FOREIGN KEY FK_16E31C451A9A7125676C7AF5');
+
+        // 2. откатываем chat_id обратно в INT
+        $this->addSql('ALTER TABLE place_messenger_user MODIFY chat_id INT NOT NULL');
+        $this->addSql('ALTER TABLE messenger_user MODIFY chat_id INT NOT NULL');
+
+        // 3. username снова NOT NULL
+        $this->addSql('ALTER TABLE messenger_user MODIFY username VARCHAR(255) NOT NULL');
+
+        // 4. возвращаем FK обратно
+        $this->addSql(
+            'ALTER TABLE place_messenger_user ADD CONSTRAINT FK_16E31C451A9A7125676C7AF5 FOREIGN KEY (chat_id, messenger_id) REFERENCES messenger_user (chat_id, messenger_id)'
+        );
     }
 }
