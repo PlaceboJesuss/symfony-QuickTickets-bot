a309ab72a1c51686bac134b481ea63ab17b728dc
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-29 19:28:41 +0300

    last

--- nginx/watch-certs.sh
+++ nginx/watch-certs.sh
@@ -4,10 +4,23 @@ CERT_DIR="/etc/letsencrypt/live/${APP_DOMAIN}"
 
 echo "👀 Watching certificate changes in $CERT_DIR"
 
+build_http() {
+    envsubst '${APP_DOMAIN}' \
+        < /etc/nginx/http.conf.template \
+        > /etc/nginx/conf.d/default.conf
+}
+
+build_https() {
+    envsubst '${APP_DOMAIN}' \
+        < /etc/nginx/https.conf.template \
+        > /etc/nginx/conf.d/default.conf
+}
+
 while [ ! -d "$CERT_DIR" ]; do
     sleep 5
 done
 
+build_https
 echo "🔁 Certificate create → reloading nginx"
 nginx -s reload || true
 
