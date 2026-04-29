6eaf40a982650abd67dc252f5c027919fffd4e12
Shevchenko Mikhail <shevchenko.m@introvert.bz>
2026-04-29 19:15:58 +0300

    gg

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 set -e
 
-echo ">>> Starting certbot loop"
+CERT_PATH="/etc/letsencrypt/live/${APP_DOMAIN}/fullchain.pem"
+
+echo ">>> Starting entrypoint..."
+
+if [ ! -f "$CERT_PATH" ]; then
+    echo ">>> No certificate found. Requesting initial cert..."
+
+    certbot certonly \
+        --webroot -w /var/www/certbot \
+        --email "$APP_EMAIL" \
+        --agree-tos \
+        --no-eff-email \
+        --non-interactive \
+        -d "$APP_DOMAIN" \
+        -d "www.$APP_DOMAIN"
+else
+    echo ">>> Certificate already exists."
+fi
+
+echo ">>> Starting renewal loop..."
 
 while true; do
-  certbot renew --webroot -w /var/www/certbot --quiet
-  sleep 12h
+    certbot renew --webroot -w /var/www/certbot --quiet
+    sleep 12h
 done
--- nginx/watch-certs.sh
+++ nginx/watch-certs.sh
@@ -3,7 +3,7 @@
 CERT_DIR="/etc/letsencrypt/live/${APP_DOMAIN}"
 
 echo "👀 Watching certificate changes in $CERT_DIR"
-sleep 5
+sleep 20
 
 while inotifywait -e modify,create,delete,move -r "$CERT_DIR"; do
     echo "🔁 Certificate changed → reloading nginx"
